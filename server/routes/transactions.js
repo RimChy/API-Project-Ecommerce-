0d608a7e73911bdc70de9d772a59ec3fe1a3a0bf
Muhid Hassan Risvy <hassan.risvy1@gmail.com>
2022-08-27 14:44:36 +0600

    Delete server directory

--- server/routes/transactions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const Transactions = require('../controllers/transactions');
-
-router.post('/transfer', Transactions.transfer);
-
-module.exports = router;
